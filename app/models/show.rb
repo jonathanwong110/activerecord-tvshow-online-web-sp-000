c568d3d8ed5c69fbc341bb4a2e19cb8c5fe2a82c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-18 22:53:36 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    Show.maximum{|s| s[:rating] }
+    show = Show.maximum(:rating)
+    show
   end
   
   def self.lowest_rating
